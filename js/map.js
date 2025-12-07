2f213c3df8cfc81917ebe3ff498f08561ecca9b4
MatthijsdHooge <ct3corp@gmail.com>
2025-12-07 01:48:38 +0100

    Revert "removed live server.js"
    This reverts commit 4a6aa03c8f28f47a1b4a2d6a74540aab9c213d43.

--- js/map.js
+++
@@ -1,5 +0,0 @@
-let map = L.map('map').setView([51.3977444, 4.4514129], 12);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 12
-}).addTo(map);
-L.marker([51.3977444, 4.4514129]).addTo(map);
